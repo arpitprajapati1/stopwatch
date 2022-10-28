c878c776d204142460d30fc6b4c69cd59982be25
arpitprajapati1 <arpitpjpt8888@gmail.com>
2022-10-28 22:18:33 +0530

    node setting update

---
+++ server.js
@@ -0,0 +1,31 @@
+const { urlencoded } = require("express");
+const path = require('path');
+const express = require("express");
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+
+app.use("/assets",express.static("public")); 
+
+// //app.use(express.static(path.join(__dirname, 'public')));
+
+// //app.use(express.static('public'));
+// // Specific folder example
+// app.use('/css', express.static(__dirname + 'public/css'))
+// // app.use('/js', express.static(__dirname + 'public/js'))
+// // app.use('/img', express.static(__dirname + 'public/images'))
+
+// // Set View's
+// app.set('views', './views');
+
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+    res.render('home');
+    console.log("this is end point");
+})
+
+app.listen(8000,()=>{
+    console.log('Running on server https://localhost:8000 ......');
+})
